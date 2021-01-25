1ce861e829a58933e1642841ce9fa4f84a829ff8
Ji <hwichance@gmail.com>
2021-01-25 21:18:17 +0900

    Add toolbar and search button to MainActivity

--- app/src/main/java/com/hwichance/android/mindblooming/MainActivity.kt
+++ app/src/main/java/com/hwichance/android/mindblooming/MainActivity.kt
@@ -2,10 +2,57 @@ package com.hwichance.android.mindblooming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainToolbar: MaterialToolbar
+    private lateinit var searchItem: MenuItem
+    private lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
+    }
+
+    private fun bindViews() {
+        mainToolbar = findViewById(R.id.mainToolbar)
+        searchItem = mainToolbar.menu.findItem(R.id.mainSearchMenu)
+        searchView = searchItem.actionView as SearchView
+
+        setSearchAction()
+        setToolbarListener()
+    }
+
+    private fun setSearchAction() {
+        searchView.queryHint = getString(R.string.main_toolbar_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                // TODO("Not yet implemented")
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                // TODO("Not yet implemented")
+                return true
+            }
+        })
+    }
+
+    private fun setToolbarListener() {
+        mainToolbar.setNavigationOnClickListener {
+
+        }
+    }
+
+    override fun onBackPressed() {
+        if (searchItem.isActionViewExpanded) {
+            searchItem.collapseActionView()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
