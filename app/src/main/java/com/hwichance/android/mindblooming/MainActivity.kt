8d278de8bacb8c308dfff97bc0beae6e28980655
Ji <hwichance@gmail.com>
2021-01-26 18:08:18 +0900

    implement navigation drawer

--- app/src/main/java/com/hwichance/android/mindblooming/MainActivity.kt
+++ app/src/main/java/com/hwichance/android/mindblooming/MainActivity.kt
@@ -2,19 +2,23 @@ package com.hwichance.android.mindblooming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.InputType
-import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainToolbar: MaterialToolbar
+    private lateinit var mainDrawerLayout: DrawerLayout
+    private lateinit var mainNavigationDrawer: NavigationView
+    private lateinit var versionTextView: TextView
     private lateinit var searchItem: MenuItem
     private lateinit var searchView: SearchView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,11 +28,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun bindViews() {
         mainToolbar = findViewById(R.id.mainToolbar)
+        mainDrawerLayout = findViewById(R.id.mainDrawerLayout)
+        mainNavigationDrawer = findViewById(R.id.mainNavigationDrawer)
+
+        versionTextView = mainNavigationDrawer.getHeaderView(0).findViewById(R.id.versionTextView)
         searchItem = mainToolbar.menu.findItem(R.id.mainSearchMenu)
         searchView = searchItem.actionView as SearchView
 
+        setVersionText()
         setSearchAction()
+        setDrawerAction()
         setToolbarListener()
+    }
+
+    private fun setVersionText() {
+        val versionText = getString(R.string.version_text)
+        versionTextView.text = (versionText + BuildConfig.VERSION_NAME)
     }
 
     private fun setSearchAction() {
@@ -49,17 +64,30 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun setDrawerAction() {
+        mainNavigationDrawer.setNavigationItemSelectedListener { menuItem ->
+
+            true
+        }
+    }
+
     private fun setToolbarListener() {
         mainToolbar.setNavigationOnClickListener {
-
+            mainDrawerLayout.openDrawer(GravityCompat.START)
         }
     }
 
     override fun onBackPressed() {
-        if (searchItem.isActionViewExpanded) {
-            searchItem.collapseActionView()
-        } else {
-            super.onBackPressed()
+        when {
+            searchItem.isActionViewExpanded -> {
+                searchItem.collapseActionView()
+            }
+            mainDrawerLayout.isDrawerOpen(GravityCompat.START) -> {
+                mainDrawerLayout.closeDrawer(GravityCompat.START)
+            }
+            else -> {
+                super.onBackPressed()
+            }
         }
     }
 }
